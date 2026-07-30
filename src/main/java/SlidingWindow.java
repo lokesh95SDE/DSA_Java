ff648dc2078761e60767a7094c83e727985f9732
Lokesh <lokesh95info@gmail.com>
2026-07-30 23:32:27 +0530

    AverageOfmaxSumFixedArray_FixedSlidingWindow

--- src/main/java/SlidingWindow.java
+++ src/main/java/SlidingWindow.java
@@ -1,10 +1,14 @@
+import java.util.*;
+
 public class SlidingWindow {
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 1, 2, 4, 3};
 ////      ****Sliding_Window_Algo****
-        AverageOfmaxSumFixedArray_FixedSlidingWindow(arr,3);
-        miniLengthArrayTargetedSum_VariableSlidingWindow(arr);
+//        AverageOfmaxSumFixedArray_FixedSlidingWindow(arr,3);
+        AverageOfmaxSumFixedArray_FixedSlidingWindowII(arr,3);
+//        miniLengthArrayTargetedSum_VariableSlidingWindow(arr);
+        findAnagrams("abcbcabcbb","abc");
     }
 
     ////*************************************************************************************************************
@@ -26,6 +30,26 @@ public class SlidingWindow {
 
     }
 
+    static double AverageOfmaxSumFixedArray_FixedSlidingWindowII(int[] nums, int k ) {
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+        int maxSum = 0;
+        int sum = 0;
+        while(right<n){
+            sum += nums[right];
+            if(right-left+1 == k){
+                maxSum = Math.max(maxSum,sum);
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        System.out.println((double)maxSum/k);
+    return (double)maxSum/k;
+    }
+
+
     static void miniLengthArrayTargetedSum_VariableSlidingWindow(int[] arr) {
         int target = 5;
         int k = 0;   ///no fixed length so every iteration assume as fixed length
@@ -44,6 +68,46 @@ public class SlidingWindow {
         System.out.println(minLength);
 
     }
+
+        public static List<Integer> findAnagrams(String s, String p) {
+            int left = 0;
+            int right = 0;
+            int n = s.length();
+            List<Integer> result =  new ArrayList<>();
+            Map<Character, Integer> map = new HashMap<>();
+            for(Character c : p.toCharArray()){
+                map.put(c,map.getOrDefault(c,0)+1);
+            }
+            int counter = map.size();
+            while(right<n){
+                char ch = s.charAt(right);
+                if(map.containsKey(ch)) {
+                    map.put(ch,map.get(ch)-1);
+                    if(map.get(ch)==0){
+                        counter--;
+                    }
+                }
+                right++;
+
+                while(counter==0){
+                    char tch = s.charAt(left);
+                    if(right-left == p.length()){
+                        result.add(left);
+                    }
+                    if(map.containsKey(tch)){
+                        map.put(tch,map.get(tch)+1);
+                        if(map.get(tch)>0){
+                            counter++;
+                        }
+                    }
+                    left++;
+                }
+            }
+            return result;
+        }
+
+
+
 
 
 }
